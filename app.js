9cc30f538bded499829e44689b7c6e51929c4757
andrsand <angorko@list.ru>
2022-10-03 21:21:26 +0300

    feature: Connecting to mongoDB
    * Creation of functionality for connecting to the cloud version of mongoDB in App.js, default.json

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const config = require('config')
-const mongoose = require('mongoose ')
+const mongoose = require('mongoose')
 
 const app = express() // app - резудльтат работы express т.e. наш сервер
 
@@ -9,14 +9,15 @@ const PORT = config.get('port') || 5000 // получаем строчку port 
 async function start() {
   try {
     await mongoose.connect(config.get('mongoUri'), {
-
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+      // useCreateIndex: true
     })
+    app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`)) // настройка порта для подключения сервер
   } catch (e) {
     console.log('Server Error', e.message)
-    process.exit(code, 1)  // если ошибка - выходим из процесса
+    process.exitCode = 1  // если ошибка - выходим из процесса
   }
 }
 
 start()
-
-app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`)) // настройка порта для подключения сервера
