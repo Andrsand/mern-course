16d3191ae332d4f08bdf92aea93573daae44dc52
andrsand <angorko@list.ru>
2022-10-03 19:52:30 +0300

    feature: Server setup
    * Creation of functionality to start the server in App.js, default.json

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const config = require('config')
+const mongoose = require('mongoose ')
 
 const app = express() // app - резудльтат работы express т.e. наш сервер
 
 const PORT = config.get('port') || 5000 // получаем строчку port из config/default.json или по умолчанию - 5000
+
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+
+    })
+  } catch (e) {
+    console.log('Server Error', e.message)
+    process.exit(code, 1)  // если ошибка - выходим из процесса
+  }
+}
+
+start()
+
 app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`)) // настройка порта для подключения сервера
