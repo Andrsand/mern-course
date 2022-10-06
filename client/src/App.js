02ec2b91b3dd6ab487bfd150b5fcf19fbeffe49c
andrsand <angorko@list.ru>
2022-10-06 14:17:30 +0300

    feature: Implementation of Frontend
    * Added file src/routs.js
    * Implemented routes in App.js and routes.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { useRoutes } from './routes'
 import 'materialize-css' // импорт стилей
 
 function App() {
+  const routes = useRoutes(false) //! добавление роутов. Если не будет работать перед (false) должно быть (isAuthenticated: false) - video 01.03.00
   return (
-    <div className='container'>
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <div className='container'>
+        {routes} {/* вставляем роуты в качестве контента*/}
+      </div>
+    </Router>
+
   )
 }
 
---
+++ client/src/routes.js
@@ -0,0 +1,59 @@
+
+import React from "react"
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { LinksPage } from "./pages/LinksPage"
+import { CreatePage } from "./pages/CreatePage"
+import { DetailPage } from "./pages/DetailPage"
+import { AuthPage } from "./pages/AuthPage"
+
+export const useRoutes = isAuthenticated => { // флаг для проверки существует ли прользователь и есть ли у него token
+    if (isAuthenticated) { // если флаг сработал выводим компонент Switch из reac-dom
+        return (
+            // <Routes> //! вариант роутинга сторой версии React
+            //     <Route path="/links" exact>
+            //         <LinksPage />
+            //     </Route>
+            //     <Route path="/create" exact>
+            //         <CreatePage />
+            //     </Route>
+            //     <Route path="/detail/:id">
+            //         <DetailPage />
+            //     </Route>
+            //     {/*<Redirect to="/create" />*/}
+            //     <Route path="/create" ></Route>
+            // </Routes>
+
+            <Routes>
+                <Route path="/links" element={<LinksPage />} />
+
+
+                <Route path="/create" element={<CreatePage />} />
+
+
+                <Route path="/detail/:id" element={<DetailPage />} />
+
+                <Route path="*" element={<CreatePage />} />
+
+            </Routes>
+
+        )
+    }
+
+    return (                // если флаг isAuthenticated не сработал и пользователь еще не в системе - выводим следующие роуты....
+        // <Routes>         //! вариант роутинга сторой версии React
+        //     <Route path="/" exact>
+        //         <AuthPage />
+        //     </Route>
+        //     {/** <Redirect to="/" />*/}
+        //     <Route path="/" ></Route>
+        // </Routes>
+
+        <Routes>
+            <Route path="/" element={<AuthPage />} />
+
+
+            {/** <Redirect to="/" />*/}
+            <Route path="/" /> {/*возможно придется изменить этот редирект */}
+        </Routes>
+    )
+}
